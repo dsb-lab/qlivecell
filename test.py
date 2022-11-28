90a36e8101ae9eec9d550974df4300b4b3ce049a
Pablo Casani Galdon <pablocasani@gmail.com>
2022-11-28 14:14:19 +0100

    mofied gitignore

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from matplotlib.widgets import Slider, Button
 
 # The parametrized function to be plotted
 def f(t, frequency):
-    return 5 * np.sin(2 * np.pi * frequency * t)
+    return 5.0 * np.sin(2 * np.pi * frequency * t)
 
 t = np.linspace(0, 1, 1000)
 
@@ -19,5 +19,23 @@ ax.set_xlabel('Time [s]')
 
 # adjust the main plot to make room for the sliders
 fig.subplots_adjust(bottom=0.25)
+
+# Make a horizontal slider to control the frequency.
+axfreq = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+freq_slider = Slider(
+    ax=axfreq,
+    label='Frequency [Hz]',
+    valmin=0.1,
+    valmax=30,
+    valinit=init_frequency,
+)
+
+# The function to be called anytime a slider's value changes
+def update(val):
+    line.set_ydata(f(t, freq_slider.val))
+    fig.canvas.draw_idle()
+
+# register the update function with each slider
+freq_slider.on_changed(update)
 
 plt.show()
