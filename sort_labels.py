4c3f4afb1ca553ad27154b91e18b967011b65537
PabloCasaniGaldon <pablocasani@gmail.com>
2022-12-13 16:50:06 +0100

    completed tests for the sort labels function

--- sort_labels.py
+++ sort_labels.py
@@ -1,20 +1,29 @@
 P = [[0,1,3,5], [0,1,2,6], [1,2,4,7]]
 def sort_labels(P):
     Q = [[-1 for item in sublist] for sublist in P]
-    keys = [item for item in range(max([sublist[-1] for sublist in P])+1)]
-    vals = [[] for key in keys]
+    C = [[] for item in range(max([sublist[-1] for sublist in P])+1)]
+    PQ = [-1 for sublist in C]
     for i, p in enumerate(P):
         for j, n in enumerate(p):
-            vals[n].append([i,j])
-    C = {keys[ii]: vals[ii] for ii in range(len(keys))}
-
+            C[n].append([i,j])
     nmax = 0
     for i, p in enumerate(P):
         for j, n in enumerate(p):
             ids = C[n]
             if Q[i][j] == -1:
                 for ij in ids:
-                        Q[ij[0]][ij[1]] = nmax
+                    Q[ij[0]][ij[1]] = nmax
+                PQ[n] = nmax
                 nmax += 1
-    return Q
-Q = sort_labels(P)
+    return Q, PQ
+Q, PQ = sort_labels(P)
+
+from copy import deepcopy
+newP = deepcopy(P)
+
+for i, p in enumerate(newP):
+    for j, n in enumerate(p):
+        newP[i][j] = PQ[n]
+
+assert(newP == Q)
+print("TEST PASSED")
