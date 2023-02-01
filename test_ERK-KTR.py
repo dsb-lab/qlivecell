e8f9130582943694bb6625c06bea9e9886f43837
PabloCasaniGaldon <pablocasani@gmail.com>
2023-02-01 18:30:23 +0100

    Generated big part of the code but it is only partially working

--- test_ERK-KTR.py
+++ test_ERK-KTR.py
@@ -6,6 +6,7 @@ sys.path.insert(0, "/home/pablo/Desktop/PhD/projects/CellTracking")
 
 from CellTracking import CellTracking
 from CellTracking import load_CT
+from ERKKTR import *
 import os
 home = os.path.expanduser('~')
 path_data=home+'/Desktop/PhD/projects/Data/blastocysts/movies/2h_claire_ERK-KTR_MKATE2/registered/'
@@ -21,3 +22,58 @@ f = embcode+'.tif'
 IMGS   = imread(path_data+f)[:4,:,0,:,:]
 
 ERK_movie = None
+
+cell  = CT.cells[0]
+donut = ERKKTR_donut(cell, innerpad=4, outterpad=2, donut_width=10)
+
+t = 0
+z = 0
+img = CT.stacks[t,z] 
+tid = cell.times.index(t)
+zid = cell.zs[tid].index(z)
+outline = cell.outlines[tid][zid]
+mask    = cell.masks[tid][zid]
+
+nuc_mask = donut.nuclei_masks[tid][zid]
+nuc_outline = donut.nuclei_outlines[tid][zid]
+#nuc_outline, _x, _y = donut._expand_hull(outline, -5)
+
+don_mask = donut.donut_masks[tid][zid]
+
+fig, ax = plt.subplots()
+#ax.imshow(img)
+ax.scatter(outline[:,0], outline[:,1], s=1)
+ax.scatter(nuc_outline[:,0], nuc_outline[:,1], s=1)
+
+#ax.scatter(don_mask[:,0], don_mask[:,1])
+plt.show()
+
+fig, ax = plt.subplots()
+#ax.imshow(img)
+ax.scatter(outline[:,0], outline[:,1], s=1)
+ax.scatter(don_mask[:,0], don_mask[:,1], s=1)
+
+#ax.scatter(don_mask[:,0], don_mask[:,1])
+plt.show()
+
+
+innteroutline, midx, midy = donut._expand_hull(outline, inc=2)
+outteroutline, midx, midy = donut._expand_hull(outline, inc=2+5)
+innteroutline=donut._increase_point_resolution(innteroutline)
+outteroutline=donut._increase_point_resolution(outteroutline)
+maskin = donut._points_within_hull(np.array(innteroutline).astype('int32'))
+maskout= donut._points_within_hull(np.array(outteroutline).astype('int32'))
+mask = np.setdiff1d(maskout, maskin)
+
+fig, ax = plt.subplots()
+#ax.imshow(img)
+ax.scatter(maskout[:,0], maskout[:,1], s=1)
+ax.scatter(maskin[:,0], maskin[:,1])
+plt.show()
+
+self.donut_masks[tid][zid] = np.array(mask)
+
+miny=0
+maxy=5
+minx=1
+maxx=3
