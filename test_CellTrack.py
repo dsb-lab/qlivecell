37abbbeb76d89671dc29bb894c3a26fa14485ecf
PabloCasaniGaldon <pablocasani@gmail.com>
2023-02-13 17:45:33 +0100

    Nothing to commit

--- test_CellTrack.py
+++ test_CellTrack.py
@@ -8,41 +8,41 @@ path_data=home+'/Desktop/PhD/projects/Data/blastocysts/movies/2h_claire_ERK-KTR_
 path_save=home+'/Desktop/PhD/projects/Data/blastocysts/CellTrackObjects/2h_claire_ERK-KTR_MKATE2/'
 
 files = os.listdir(path_data)
-emb = 9
-embcode=files[emb].split('.')[0]
-IMGS   = [imread(path_data+f)[:3,:,1,:,:] for f in files[emb:emb+1]][0]
-model  = models.CellposeModel(gpu=True, pretrained_model='/home/pablo/Desktop/PhD/projects/Data/blastocysts/movies/2h_claire_ERK-KTR_MKATE2/cell_tracking/training_set_expanded_nuc/models/blasto')
-#model  = models.Cellpose(gpu=True, model_type='nuclei')
+for emb in range(len(files)):
+    embcode=files[emb].split('.')[0]
+    IMGS   = [imread(path_data+f)[:,:,1,:,:] for f in files[emb:emb+1]][0]
+    model  = models.CellposeModel(gpu=True, pretrained_model='/home/pablo/Desktop/PhD/projects/Data/blastocysts/movies/2h_claire_ERK-KTR_MKATE2/cell_tracking/training_set_expanded_nuc/models/blasto')
+    #model  = models.Cellpose(gpu=True, model_type='nuclei')
 
-CT = CellTracking( IMGS, model, path_save, embcode
-                    , trainedmodel=True
-                    , channels=[0,0]
-                    , flow_th_cellpose=0.4
-                    , distance_th_z=3.0
-                    , xyresolution=0.2767553 # microns per pixel
-                    , zresolution =2
-                    , relative_overlap=False
-                    , use_full_matrix_to_compute_overlap=True
-                    , z_neighborhood=2
-                    , overlap_gradient_th=0.15
-                    , plot_layout=(2,2)
-                    , plot_overlap=1
-                    , masks_cmap='tab10'
-                    , min_outline_length=400
-                    , neighbors_for_sequence_sorting=7
-                    , plot_tracking_windows=1
-                    , backup_steps=20
-                    , time_step=5 # minutes
-                    , cell_distance_axis="xy"
-                    , movement_computation_method="center"
-                    , mean_substraction_cell_movement=False)
+    CT = CellTracking( IMGS, model, path_save, embcode
+                        , trainedmodel=True
+                        , channels=[0,0]
+                        , flow_th_cellpose=0.4
+                        , distance_th_z=3.0
+                        , xyresolution=0.2767553 # microns per pixel
+                        , zresolution =2
+                        , relative_overlap=False
+                        , use_full_matrix_to_compute_overlap=True
+                        , z_neighborhood=2
+                        , overlap_gradient_th=0.15
+                        , plot_layout=(2,2)
+                        , plot_overlap=1
+                        , masks_cmap='tab10'
+                        , min_outline_length=400
+                        , neighbors_for_sequence_sorting=7
+                        , plot_tracking_windows=1
+                        , backup_steps=20
+                        , time_step=5 # minutes
+                        , cell_distance_axis="xy"
+                        , movement_computation_method="center"
+                        , mean_substraction_cell_movement=False)
 
-CT()
-# CT.plot_tracking()
-# CT.plot_cell_movement()
-# CT.plot_masks3D_Imagej(cell_selection=True)
-save_CT(CT, path_save, embcode)
-cells, CT_info = load_CT(path_save, embcode)
+    CT()
+    # CT.plot_tracking()
+    # CT.plot_cell_movement()
+    # CT.plot_masks3D_Imagej(cell_selection=True)
+    save_CT(CT, path_save, embcode)
+    cells, CT_info = load_CT(path_save, embcode)
 #CT.plot_tracking(windows=1, plot_layout=(2,2), plot_overlap=1, masks_cmap='tab10')
 #CT.plot_cell_movement(substract_mean=False, plot_layout=(2,2), plot_overlap=1, masks_cmap='tab10', movement_computation_method="all_to_all")
 #CT.plot_masks3D_Imagej(verbose=False, cell_selection=True, plot_layout=(2,2), plot_overlap=1, masks_cmap='tab10', keep=False)
