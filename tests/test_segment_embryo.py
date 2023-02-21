ec68e305246eb5a18a97f2a17c01dc38186644c2
PabloCasaniGaldon <pablocasani@gmail.com>
2023-02-21 11:37:55 +0100

    Embryo segmentation working as expected. Ready to merge

--- tests/test_segment_embryo.py
+++ tests/test_segment_embryo.py
@@ -1,15 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage.segmentation import (morphological_chan_vese,
-                                  checkerboard_level_set)
-
+from skimage.segmentation import morphological_chan_vese, checkerboard_level_set
 from cellpose.io import imread
+import os
 import sys
 sys.path.insert(0, "/home/pablo/Desktop/PhD/projects/CellTracking")
-
-import os
-
-import numpy as np
+from utils_ERKKTR import gkernel, convolve2D 
 
 home = os.path.expanduser('~')
 path_data=home+'/Desktop/PhD/projects/Data/blastocysts/movies/2h_claire_ERK-KTR_MKATE2/registered/'
@@ -28,64 +24,16 @@ IMGS_SEG   = imread(path_data+f)[:4,:,1,:,:]
 t = 2
 z = 20
 
-def gkernel(size, sigma):
-    x, y = np.mgrid[-size//2 + 1:size//2 + 1, -size//2 + 1:size//2 + 1]
-    g = np.exp(-((x**2 + y**2)/(2.0*sigma**2)))
-    return g/g.sum()
-
-def convolve2D(image, kernel, padding=0, strides=1):
-    # Cross Correlation
-    kernel = np.flipud(np.fliplr(kernel))
-
-    # Gather Shapes of Kernel + Image + Padding
-    xKernShape = kernel.shape[0]
-    yKernShape = kernel.shape[1]
-    xImgShape = image.shape[0]
-    yImgShape = image.shape[1]
-
-    # Shape of Output Convolution
-    xOutput = int(((xImgShape - xKernShape + 2 * padding) / strides) + 1)
-    yOutput = int(((yImgShape - yKernShape + 2 * padding) / strides) + 1)
-    output = np.zeros((xOutput, yOutput))
-
-    # Apply Equal Padding to All Sides
-    if padding != 0:
-        imagePadded = np.zeros((image.shape[0] + padding*2, image.shape[1] + padding*2))
-        imagePadded[int(padding):int(-1 * padding), int(padding):int(-1 * padding)] = image
-        print(imagePadded)
-    else:
-        imagePadded = image
-
-    # Iterate through image
-    for y in range(image.shape[1]):
-        # Exit Convolution
-        if y > image.shape[1] - yKernShape:
-            break
-        # Only Convolve if y has gone down by the specified Strides
-        if y % strides == 0:
-            for x in range(image.shape[0]):
-                # Go to next row once kernel is out of bounds
-                if x > image.shape[0] - xKernShape:
-                    break
-                try:
-                    # Only Convolve if x has moved by the specified Strides
-                    if x % strides == 0:
-                        output[x, y] = (kernel * imagePadded[x: x + xKernShape, y: y + yKernShape]).sum()
-                except:
-                    break
-
-    return output
-
-# Morphological GAC
 image = IMGS_ERK[t][z] 
 ksize  = 5
 ksigma = 3
+binths = 8
 
 kernel = gkernel(ksize, ksigma)
 convimage  = convolve2D(image, kernel, padding=10)
 cut=int((convimage.shape[0] - image.shape[0])/2)
 convimage=convimage[cut:-cut, cut:-cut]
-binimage = (convimage > 8)*1
+binimage = (convimage > binths)*1
 
 # Morphological ACWE
 
@@ -123,6 +71,3 @@ ax[1].set_title("Morphological ACWE - background", fontsize=12)
 
 fig.tight_layout()
 plt.show()
-
-
-
