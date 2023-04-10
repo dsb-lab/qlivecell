436b44d92a38754733e5c22e7a0a9762ac555462
Pablo Casani Galdon <pablocasani@gmail.com>
2023-04-10 12:05:13 +0200

    changed file imgage upload

--- test_ERK-KTR.py
+++ test_ERK-KTR.py
@@ -4,34 +4,32 @@ import sys
 sys.path.insert(0, "/home/pablo/Desktop/PhD/projects/CellTracking")
 
 # from CellTracking import CellTracking
-from CellTracking import load_CT
+from CellTracking import load_cells, read_img_with_resolution
 
 from ERKKTR import *
 import os
 
 home = os.path.expanduser('~')
-path_data=home+'/Desktop/PhD/projects/Data/blastocysts/movies/2h_claire_ERK-KTR_MKATE2/registered/'
-path_save=home+'/Desktop/PhD/projects/Data/blastocysts/CellTrackObjects/2h_claire_ERK-KTR_MKATE2/'
-emb=4
+path_data=home+'/Desktop/PhD/projects/Data/blastocysts/2h_claire_ERK-KTR_MKATE2/movies/registered/'
+path_save=home+'/Desktop/PhD/projects/Data/blastocysts/2h_claire_ERK-KTR_MKATE2/CellTrackObjects'
+
 files = os.listdir(path_data)
+embs = []
+for emb, file in enumerate(files):
+    if "082119_p1" in file: embs.append(emb)
 
-embcode=files[emb].split('.')[0]
-if "_info" in embcode: 
-    embcode=embcode[0:-5]
+emb = embs[0]
+file = files[emb]
+embcode=file.split('.')[0]
 
-f = embcode+'.tif'
+IMGS_SEG, xyres, zres = read_img_with_resolution(path_data+file, channel=1)
+IMGS_ERK, xyres, zres = read_img_with_resolution(path_data+file, channel=0)
 
-IMGS_ERK   = imread(path_data+f)[:1,:,0,:,:]
-IMGS_SEG   = imread(path_data+f)[:1,:,1,:,:]
+cells, CT_info = load_cells(path_save, embcode)
 
-cells, CT_info = load_CT(path_save, embcode)
-
-t = 0
-z = 10
-
-EmbSeg = EmbryoSegmentation(IMGS_ERK, ksize=5, ksigma=3, binths=[20,5], checkerboard_size=6, num_inter=100, smoothing=5, trange=range(t+1), zrange=range(z,z+1))
+EmbSeg = EmbryoSegmentation(IMGS_ERK, ksize=5, ksigma=3, binths=[20,10], checkerboard_size=6, num_inter=100, smoothing=5, trange=range(0+1), zrange=None, mp_threads=10)
 EmbSeg()
-EmbSeg.plot_segmentation(t, z, extra_IMGS=IMGS_SEG)
+EmbSeg.plot_segmentation(0, 23, extra_IMGS=IMGS_ERK)
 
 # save_ES(EmbSeg, path_save, embcode)
 
@@ -45,6 +43,8 @@ erkktr.create_donuts(EmbSeg)
 elapsed = time.time() - tt
 print("TIME =", elapsed)
 
+t=0
+z=15
 erkktr.plot_donuts(IMGS_SEG, IMGS_ERK, t, z, plot_nuclei=False, plot_outlines=False, plot_donut=True, EmbSeg=EmbSeg)
 
 img = IMGS_ERK[t][z] 
