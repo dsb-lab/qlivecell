4e3bcaf8cf18cbdfcdc3587ed03083c48f722dc2
PabloCasaniGaldon <pablocasani@gmail.com>
2023-04-11 12:03:04 +0200

    initial commit

--- compute_ERK-KTR.py
+++ compute_ERK-KTR.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, "/home/pablo/Desktop/PhD/projects/CellTracking")
 
 # from CellTracking import CellTracking
-from CellTracking import load_CT
+from CellTracking import load_cells
 
 from ERKKTR import *
 import os
@@ -11,21 +11,23 @@ import os
 
 if __name__ == '__main__':
     home = os.path.expanduser('~')
-    path_data=home+'/Desktop/PhD/projects/Data/blastocysts/movies/2h_claire_ERK-KTR_MKATE2/registered/'
-    path_save=home+'/Desktop/PhD/projects/Data/blastocysts/CellTrackObjects/2h_claire_ERK-KTR_MKATE2/'
-    emb=9
-    files = os.listdir(path_data)
+    path_data=home+'/Desktop/PhD/projects/Data/blastocysts/2h_claire_ERK-KTR_MKATE2/movies/registered/'
+    path_save=home+'/Desktop/PhD/projects/Data/blastocysts/2h_claire_ERK-KTR_MKATE2/CellTrackObjects'
 
+    files = os.listdir(path_data)
+    embs = []
+    for emb, file in enumerate(files):
+        if "082119_p1" in file: embs.append(emb)
     embcode=files[emb].split('.')[0]
     if "_info" in embcode: 
         embcode=embcode[0:-5]
 
     f = embcode+'.tif'
 
-    IMGS_ERK   = imread(path_data+f)[:1,:,0,:,:]
-    IMGS_SEG   = imread(path_data+f)[:1,:,1,:,:]
+    IMGS_ERK   = imread(path_data+f)[:,:,0,:,:]
+    IMGS_SEG   = imread(path_data+f)[:,:,1,:,:]
 
-    cells, CT_info = load_CT(path_save, embcode)
+    cells, CT_info = load_cells(path_save, embcode)
     # EmbSeg = EmbryoSegmentation(IMGS_ERK, ksize=5, ksigma=3, binths=[20,5], checkerboard_size=6, num_inter=100, smoothing=5, trange=range(1), mp_threads=10)
     # EmbSeg()
     # save_ES(EmbSeg, path_save, embcode)
@@ -33,13 +35,12 @@ if __name__ == '__main__':
     EmbSeg = load_ES(path_save, embcode)
     EmbSeg.plot_segmentation(0,20)
 
-    # # erkktr = load_donuts(path_save, embcode)
-    # start = time.time()
-    # erkktr = ERKKTR(IMGS_ERK, innerpad=1, outterpad=2, donut_width=8, min_outline_length=100, cell_distance_th=50.0, mp_threads=12)
-    # erkktr.create_donuts(cells, EmbSeg)
-    # end = time.time()
-    # print(end - start)
+    # erkktr = load_donuts(path_save, embcode)
+    start = time.time()
+    erkktr = ERKKTR(IMGS_ERK, innerpad=1, outterpad=2, donut_width=6, min_outline_length=100, cell_distance_th=50.0, mp_threads=None)
+    erkktr.create_donuts(cells, EmbSeg)
+    end = time.time()
+    print(end - start)
 
-    # erkktr.plot_donuts(cells, IMGS_SEG, IMGS_ERK, 0, 10, plot_nuclei=False, plot_outlines=True, plot_donut=True, EmbSeg=EmbSeg)
-    # # save_donuts(erkktr, path_save, embcode)
-
+    erkktr.plot_donuts(cells, IMGS_SEG, IMGS_ERK, 5, 10, plot_nuclei=False, plot_outlines=True, plot_donut=True, EmbSeg=EmbSeg)
+    # save_donuts(erkktr, path_save, embcode)
