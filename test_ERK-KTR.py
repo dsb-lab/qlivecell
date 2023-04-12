58461c0339a6772b8202975ef42c6653ba769aab
Pablo Casani Galdon <pablocasani@gmail.com>
2023-04-12 09:30:38 +0200

    error while importing file

--- test_ERK-KTR.py
+++ test_ERK-KTR.py
@@ -16,9 +16,11 @@ files = os.listdir(path_data)
 embs = []
 for emb, file in enumerate(files):
     if "082119_p1" in file: embs.append(emb)
+emb=embs[0]
 embcode=files[emb].split('.')[0]
 if "_info" in embcode: 
     embcode=embcode[0:-5]
+file = embcode+'.tif'
 
 IMGS_SEG, xyres, zres = read_img_with_resolution(path_data+file, channel=1)
 IMGS_ERK, xyres, zres = read_img_with_resolution(path_data+file, channel=0)
@@ -28,6 +30,8 @@ cells, CT_info = load_cells(path_save, embcode)
 EmbSeg = load_ES(path_save, embcode)
 
 erkktr = load_donuts(path_save, embcode)
+
+# erkktr.plot_donuts(cells, IMGS_SEG, IMGS_ERK, 0, 11, plot_nuclei=True, plot_outlines=False, plot_donut=True, EmbSeg=None)
 
 compute_ERK_traces(IMGS_ERK, cells, erkktr)
 
@@ -42,7 +46,7 @@ mito_cell_ids = [x[0] for y in CT_info.mito_cells for x in y]
 mito_times    = np.unique([x[0][1] for x in CT_info.mito_cells])
 
 import matplotlib.pyplot as plt
-fig, ax = plt.subplots(2,1, figsize=(10,10))
+fig, ax = plt.subplots(1,2, figsize=(10,5))
 maxerk = 0
 maxdisp = 0
 for cell in cells:
@@ -100,3 +104,35 @@ plt.show()
 #         tax.legend(loc=2)
 #         ccell +=1
 #     plt.show()
+
+
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots( figsize=(10,10))
+maxerk = 0
+maxdisp = 0
+for cell in cells:
+    if cell.id in mito_cell_ids: continue
+    if cell.id in apo_cell_ids: continue
+    if cell.fate[-1] =="TE": cc = [0.75, 0.75, 0]
+    elif cell.fate[-1] == "ICM": cc = "purple"
+    else: cc="brown"
+    # if cc!="purple":continue
+    ax.set_title("ERK-KTR trace")
+    ax.plot(cell.times, cell.ERKtrace, marker=None, color = cc)
+    # ax[1].set_ylim(0, 10)
+    maxdisp = max(maxdisp, max(cell.disp))
+    maxerk = max(maxerk, max(cell.ERKtrace))
+
+for at in apo_times:
+    x = np.ones(100)*at
+    y = np.linspace(0, stop=maxerk+0.1, num = len(x))
+    ax.plot(x,y, linewidth=3, color='k')
+
+for am in mito_times:
+    x = np.ones(100)*am
+    y = np.linspace(0, stop=maxerk, num = len(x))
+    ax.plot(x,y, linewidth=3, color='green', linestyle='--')
+
+# plt.legend()
+plt.show()
