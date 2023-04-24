8fd0f3eea33424d7b1a3c38f07e3c9911f9d229f
PabloCasaniGaldon <pablocasani@gmail.com>
2023-04-24 17:27:24 +0200

    renamed IMGs

--- compute_ERK-KTR.py
+++ compute_ERK-KTR.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     path_save=home+'/Desktop/PhD/projects/Data/gastruloids/joshi/competition/lightsheet/CellTrackObjects/'
 
     files = os.listdir(path_data)
-    file = files[0]
+    file = files[2]
     embcode=file.split('.')[0]
 
     IMGS_F3, xyres, zres = read_img_with_resolution(path_data+file, channel=1)
@@ -28,16 +28,18 @@ if __name__ == '__main__':
 
     EmbSeg = load_ES(path_save, embcode)
 
-    erkktrF3 = ERKKTR(IMGS_F3, innerpad=1, outterpad=1, donut_width=6, min_outline_length=100, cell_distance_th=100.0, mp_threads=14)
-    erkktrF3.create_donuts(cellsF3, EmbSeg)
-    # erkktrF3 = load_donuts( path_save, embcode+'_F3')
-    erkktrF3.plot_donuts(cellsF3, IMGS_F3, IMGS_APO, 5, 33, plot_nuclei=False, plot_outlines=False, plot_donut=True, EmbSeg=None)
-    save_donuts(erkktrF3, path_save, embcode+'_F3')
+    EmbSeg.plot_segmentation([0,0,0],[10,30,60], extra_IMGS=IMGS_APO, extra_title="Apoptotic marker")
 
-    erkktrA12 = ERKKTR(IMGS_KO, innerpad=1, outterpad=1, donut_width=6, min_outline_length=100, cell_distance_th=100.0, mp_threads=14)
-    erkktrA12.create_donuts(cellsKO, EmbSeg)
-    # erkktrF3 = load_donuts( path_save, embcode+'_F3')
-    erkktrA12.plot_donuts(cellsKO, IMGS_KO, IMGS_APO, 5, 33, plot_nuclei=True, plot_outlines=False, plot_donut=True, EmbSeg=None)
-    save_donuts(erkktrA12, path_save, embcode+'_KO')
+    # erkktrF3 = ERKKTR(IMGS_F3, innerpad=1, outterpad=1, donut_width=6, min_outline_length=100, cell_distance_th=100.0, mp_threads=14)
+    # erkktrF3.create_donuts(cellsF3, EmbSeg)
+    # # erkktrF3 = load_donuts( path_save, embcode+'_F3')
+    # erkktrF3.plot_donuts(cellsF3, IMGS_F3, IMGS_APO, 5, 33, plot_nuclei=False, plot_outlines=False, plot_donut=True, EmbSeg=None)
+    # save_donuts(erkktrF3, path_save, embcode+'_F3')
+
+    # erkktrA12 = ERKKTR(IMGS_KO, innerpad=1, outterpad=1, donut_width=6, min_outline_length=100, cell_distance_th=100.0, mp_threads=14)
+    # erkktrA12.create_donuts(cellsKO, EmbSeg)
+    # # erkktrF3 = load_donuts( path_save, embcode+'_F3')
+    # erkktrA12.plot_donuts(cellsKO, IMGS_KO, IMGS_APO, 5, 33, plot_nuclei=True, plot_outlines=False, plot_donut=True, EmbSeg=None)
+    # save_donuts(erkktrA12, path_save, embcode+'_KO')
     
 ## TO DO: show errors or warnings when donuts are too big for the given data. Either that or delete problematic cells
