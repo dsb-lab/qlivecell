8a16a0c0e40b4b0048a8d6a14ddacfe5727771bb
Pablo Casani Galdon <pablocasani@gmail.com>
2023-04-24 09:27:07 +0200

    gastruloid tests

--- compute_ERK-KTR.py
+++ compute_ERK-KTR.py
@@ -1,6 +1,7 @@
 from cellpose.io import imread
 import sys
 sys.path.insert(0, "/home/pablo/Desktop/PhD/projects/CellTracking")
+sys.path.insert(0, "/home/pablo/Desktop/PhD/projects/EmbryoSegmentation")
 
 from CellTracking import load_cells, read_img_with_resolution
 
@@ -10,38 +11,33 @@ import os
 
 if __name__ == '__main__':
     home = os.path.expanduser('~')
-    path_data=home+'/Desktop/PhD/projects/Data/blastocysts/2h_claire_ERK-KTR_MKATE2/movies/registered/'
-    path_save=home+'/Desktop/PhD/projects/Data/blastocysts/2h_claire_ERK-KTR_MKATE2/CellTrackObjects'
+    path_data=home+'/Desktop/PhD/projects/Data/gastruloids/joshi/competition/lightsheet/movies_registered/'
+    path_save=home+'/Desktop/PhD/projects/Data/gastruloids/joshi/competition/lightsheet/CellTrackObjects/'
 
     files = os.listdir(path_data)
-    embs = []
-    for emb, file in enumerate(files):
-        if "082119_p1" in file: embs.append(emb)
-    embcode=files[emb].split('.')[0]
-    if "_info" in embcode: 
-        embcode=embcode[0:-5]
+    file = files[0]
+    embcode=file.split('.')[0]
 
-    file = embcode+'.tif'
+    IMGS_F3, xyres, zres = read_img_with_resolution(path_data+file, channel=1)
+    IMGS_KO, xyres, zres = read_img_with_resolution(path_data+file, channel=0)
 
-    _IMGS_SEG, xyres, zres = read_img_with_resolution(path_data+file, channel=1)
-    _IMGS_ERK, xyres, zres = read_img_with_resolution(path_data+file, channel=0)
+    IMGS_APO, xyres, zres = read_img_with_resolution(path_data+file, channel=2)
 
-    IMGS_SEG = _IMGS_SEG[:,:,:,:]
-    IMGS_ERK = _IMGS_ERK[:,:,:,:]
-
-    cells, CT_info = load_cells(path_save, embcode)
+    cellsF3, CT_infoF3 = load_cells(path_save, embcode+'_ch%d' %1)
+    cellsKO, CT_infoKO = load_cells(path_save, embcode+'_ch%d' %0)
 
     EmbSeg = load_ES(path_save, embcode)
-    # EmbSeg.plot_segmentation(0,20)
 
-    # erkktr = load_donuts(path_save, embcode)
-    # start = time.time()
-    erkktr = ERKKTR(IMGS_ERK, innerpad=1, outterpad=1, donut_width=6, min_outline_length=100, cell_distance_th=100.0, mp_threads=10)
-    erkktr.create_donuts(cells, EmbSeg)
-    # end = time.time()
-    # print(end - start)
+    erkktrF3 = ERKKTR(IMGS_F3, innerpad=1, outterpad=1, donut_width=6, min_outline_length=100, cell_distance_th=100.0, mp_threads=14)
+    erkktrF3.create_donuts(cellsF3, EmbSeg)
+    # erkktrF3 = load_donuts( path_save, embcode+'_F3')
+    erkktrF3.plot_donuts(cellsF3, IMGS_F3, IMGS_APO, 5, 33, plot_nuclei=False, plot_outlines=False, plot_donut=True, EmbSeg=None)
+    save_donuts(erkktrF3, path_save, embcode+'_F3')
 
-    erkktr.plot_donuts(cells, _IMGS_SEG, _IMGS_ERK, 0, 15, plot_nuclei=True, plot_outlines=False, plot_donut=True, EmbSeg=None)
-    save_donuts(erkktr, path_save, embcode)
-
+    erkktrA12 = ERKKTR(IMGS_KO, innerpad=1, outterpad=1, donut_width=6, min_outline_length=100, cell_distance_th=100.0, mp_threads=14)
+    erkktrA12.create_donuts(cellsKO, EmbSeg)
+    # erkktrF3 = load_donuts( path_save, embcode+'_F3')
+    erkktrA12.plot_donuts(cellsKO, IMGS_KO, IMGS_APO, 5, 33, plot_nuclei=True, plot_outlines=False, plot_donut=True, EmbSeg=None)
+    save_donuts(erkktrA12, path_save, embcode+'_KO')
+    
 ## TO DO: show errors or warnings when donuts are too big for the given data. Either that or delete problematic cells
