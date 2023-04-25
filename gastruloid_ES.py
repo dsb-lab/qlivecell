9cbe85aebcd8e069d0d8f05e4bfb1c7257e13704
Pablo Casani Galdon <pablocasani@gmail.com>
2023-04-25 10:32:22 +0200

    .

--- gastruloid_ES.py
+++ gastruloid_ES.py
@@ -8,7 +8,6 @@ home = os.path.expanduser('~')
 path_data=home+'/Desktop/PhD/projects/Data/gastruloids/joshi/competition/lightsheet/movies_registered/'
 path_save=home+'/Desktop/PhD/projects/Data/gastruloids/joshi/competition/lightsheet/CellTrackObjects/'
 
-
 file, embcode = get_file_embcode(path_data, 0)
 
 IMGS_KO, xyres, zres  = read_img_with_resolution(path_data+file, channel=0)
@@ -24,7 +23,7 @@ IMGS = IMGS.astype('uint8')
 EmbSeg = EmbryoSegmentation(IMGS, ksize=5, ksigma=3, binths=[15,25], checkerboard_size=6, num_inter=100, smoothing=5, trange=None, zrange=None, mp_threads=14, apply_biths_to_zrange_only=False)
 EmbSeg(IMGS)
 
-# save_ES(EmbSeg, path_save, embcode)
+save_ES(EmbSeg, path_save, embcode)
 # EmbSeg = load_ES(path_save, embcode)
 
 EmbSeg.plot_segmentation([0,0,0],[3,33,62], plot_background=True, extra_IMGS=IMGS_APO, extra_title="Apoptotic marker")
