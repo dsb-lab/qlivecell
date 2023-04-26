bd873477e6f048ee9f33148c8e8db08679b65619
PabloCasaniGaldon <pablocasani@gmail.com>
2023-04-26 16:39:24 +0200

    Added some documentation

--- utils/utils_general.py
+++ utils/utils_general.py
@@ -6,13 +6,46 @@ import numpy as np
 
 import shutil
 
-def get_file_embcode(path_data, emb):
+def get_file_embcode(path_data, f):
+    """
+    Parameters
+    ----------
+    path_data : str
+        The path to the directory containing emb
+    f : str or int
+        if str returns path_data/emb
+        if int returns the emb element in path_data
+    
+    Returns
+    -------
+    file, name
+        full file path and file name.
+    """    
     files = os.listdir(path_data)
-    file = files[emb]
-    embcode=file.split('.')[0]
-    return file, embcode
+    if isinstance(f, str):
+        for i, file in enumerate(files): 
+            if f in file: fid=i
+    else: fid=f
+    file = files[fid]
+    name=file.split('.')[0]
+    return file, name
+
 
 def read_img_with_resolution(path_to_file, channel=0):
+    """
+    Parameters
+    ----------
+    path_to_file : str
+        The path to the tif file.
+    channel : int or None
+        if None assumes the tif file contains only one channel
+        if int selects that channel from the tif
+    
+    Returns
+    -------
+    IMGS, xyres, zres
+        4D numpy array with shape (t, z, x, y), x and y resolution and z resolution
+    """  
     with TiffFile(path_to_file) as tif:
         preIMGS = tif.asarray()
         shapeimg = preIMGS.shape
