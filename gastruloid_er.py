570feaec93e68a0f81353f5271c2dfe7ccecf047
PabloCasaniGaldon <pablocasani@gmail.com>
2023-05-10 17:44:57 +0200

    For now, images have to be 32 bit

--- gastruloid_er.py
+++ gastruloid_er.py
@@ -6,7 +6,7 @@ path_parent = home+'/Desktop/PhD/projects/Data/gastruloids/joshi/competition/lig
 path_data=path_parent+'movies/'
 path_save=path_parent+'CellTrackObjects/'
 
-file, embcode = get_file_embcode(path_data, 0)
+file, embcode = get_file_embcode(path_data, 1)
 
 # Extract both channels
 IMGS_ch0, xyres, zres = read_img_with_resolution(path_data+file, channel=0)
@@ -14,19 +14,19 @@ IMGS_ch1, xyres, zres = read_img_with_resolution(path_data+file, channel=1)
 IMGS_ch2, xyres, zres = read_img_with_resolution(path_data+file, channel=2)
 
 # Combine channels into single stack
-IMGS = IMGS_ch0.astype('uint16') + IMGS_ch1.astype('uint16') #+ IMGS_ch2.astype('uint16')
+IMGS = IMGS_ch0.astype('int32') + IMGS_ch1.astype('int32') #+ IMGS_ch2.astype('uint16')
 t, z, x, y = np.where(IMGS>255)
 IMGS[t,z,x,y] = 255
-IMGS = IMGS.astype('uint8')
+IMGS = IMGS.astype('int32')
 
 ### PREPROCESSING ###
 # Run centroid correction prior to Fijiyama registration to improve performance
 # IMGS_corrected = centroid_correction_3d_based_on_mid_plane(IMGS)
 # Check whether correction is good enough
-err = test_mid_plane_centroid_correction(IMGS_corrected, 0, pixel_tolerance=1)
+# err = test_mid_plane_centroid_correction(IMGS_corrected, 0, pixel_tolerance=1)
 
 # IMGS_corrected = IMGS_corrected.astype('uint8')
-IMGS_corrected = IMGS.astype('uint8')
+IMGS_corrected = IMGS.astype('float32')
 ### FJIYAMA REGISTRATION ###
 # Create Fijiyama file system (input and output folders)
 path_registered, path_output, path_movies_reg = generate_fijiyama_file_system(path_parent, 'movies_registered', embcode)
