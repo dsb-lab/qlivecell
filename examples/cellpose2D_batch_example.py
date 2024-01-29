7f5d8838b57186236a2c1826b53d5fdf817ecffe
PabloCasaniGaldon <pablocasani@gmail.com>
2024-01-29 20:41:22 +0100

    Added tiff_reader_5D to avoid having to specifically say if the expected image is stack or has channels. Everything should be on the image metadata for the functiont o be able to read it

--- examples/cellpose2D_batch_example.py
+++ examples/cellpose2D_batch_example.py
@@ -1,14 +1,14 @@
 ### LOAD PACKAGE ###
 import numpy as np
 import matplotlib.pyplot as plt
-
 from embdevtools import get_file_embcode, read_img_with_resolution, CellTracking, save_3Dstack, save_4Dstack, get_file_names, save_4Dstack_labels
 
 ### PATH TO YOU DATA FOLDER AND TO YOUR SAVING FOLDER ###
 
 embcode = '20230607_CAG_H2B_GFP_16_cells_stack2_registered'
+
 path_data='/home/pablo/Desktop/PhD/projects/Data/blastocysts/Lana/20230607_CAG_H2B_GFP_16_cells/stack_2_channel_0_obj_bottom/crop/'+embcode
-path_save='/home/pablo/Desktop/PhD/projects/Data/blastocysts/Lana/20230607_CAG_H2B_GFP_16_cells/stack_2_channel_0_obj_bottom/crop/ctobjects_test/'
+path_save='/home/pablo/Desktop/PhD/projects/Data/blastocysts/Lana/20230607_CAG_H2B_GFP_16_cells/stack_2_channel_0_obj_bottom/crop/ctobjects/'
 
 try: 
     files = get_file_names(path_save)
@@ -17,19 +17,31 @@ except:
     os.mkdir(path_save)
 
 
-### LOAD CELLPOSE MODEL ###
-from cellpose import models
-model  = models.CellposeModel(gpu=True, pretrained_model='/home/pablo/Desktop/PhD/projects/Data/blastocysts/models/blasto')
-
+### LOAD STARDIST MODEL ###
+from stardist.models import StarDist2D
+model = StarDist2D.from_pretrained('2D_versatile_fluo')
 
 ### DEFINE ARGUMENTS ###
 segmentation_args={
-    'method': 'cellpose2D', 
+    'method': 'stardist2D', 
     'model': model, 
-    # 'blur': [5,1], 
-    'channels': [0,0],
-    'flow_threshold': 0.4,
+    'blur': [10,1], 
+    # 'scale': 3
 }
+
+# ### LOAD CELLPOSE MODEL ###
+# from cellpose import models
+# model  = models.CellposeModel(gpu=True, pretrained_model='/home/pablo/Desktop/PhD/projects/Data/blastocysts/models/blasto')
+
+
+# ### DEFINE ARGUMENTS ###
+# segmentation_args={
+#     'method': 'cellpose2D', 
+#     'model': model, 
+#     # 'blur': [5,1], 
+#     'channels': [0,0],
+#     'flow_threshold': 0.4,
+# }
 
 concatenation3D_args = {
     'distance_th_z': 3.0, 
@@ -37,7 +49,7 @@ concatenation3D_args = {
     'use_full_matrix_to_compute_overlap':True, 
     'z_neighborhood':2, 
     'overlap_gradient_th':0.3, 
-    'min_cell_planes': 3,
+    'min_cell_planes': 5,
 }
 
 tracking_args = {
@@ -61,7 +73,7 @@ error_correction_args = {
 }
 
 batch_args = {
-    'batch_size': 3,
+    'batch_size': 10,
     'batch_overlap':1,
 }
 
--- src/embdevtools/celltrack/core/tools/input_tools.py
+++ src/embdevtools/celltrack/core/tools/input_tools.py
@@ -150,3 +150,76 @@ def read_img_with_resolution(path_to_file, channels=None, stack=True):
         else:
             xyres = (xres, yres)
     return IMGS, xyres, zres
+
+def tif_reader_5D(path_to_file):
+    """
+    Parameters
+    ----------
+    path_to_file : str
+        The path to the tif file.
+
+    Returns
+    -------
+    hyperstack: 
+        5D numpy array with shape (t, z, c, x, y)
+    metadata:
+        Dict containing imagej metadata and xy and z spacings (inverse of resolution)
+        
+    """
+    with TiffFile(path_to_file) as tif:
+        hyperstack = tif.asarray()
+        imagej_metadata = tif.imagej_metadata
+        tags = tif.pages[0].tags
+        
+        metadata_keys = imagej_metadata.keys()
+        
+        try: 
+            frames = imagej_metadata['frames']
+        except KeyError:
+            frames = 1
+        
+        try:
+            slices = imagej_metadata['slices']
+        except KeyError:
+            slices = 1
+        
+        try: 
+            channels = imagej_metadata['channels']
+        except KeyError:
+            channels=1
+
+        hyperstack = np.reshape(hyperstack, (frames, slices, channels, *hyperstack.shape[-2:]))
+
+        # parse X, Y resolution
+        try:
+            npix, unit = tags["XResolution"].value
+            xres = unit / npix
+        except KeyError:
+            xres = 1
+
+        try:
+            npix, unit = tags["YResolution"].value
+            yres = unit / npix
+        except KeyError:
+            yres = 1
+
+
+        try:
+            res_unit= tags["ResolutionUnit"].value
+        except KeyError:
+            yres = 1
+
+        try:
+            zres = imagej_metadata["spacing"]
+        except:
+            zres = 1
+
+        if xres == yres:
+            xyres = xres
+        else:
+            xyres = np.mean([xres, yres])
+    
+    imagej_metadata["xyres"] = xyres
+    imagej_metadata["zres"]  = zres
+    imagej_metadata["res_unit"] = res_unit
+    return hyperstack, imagej_metadata
