af62c35f7c351eb3e6caf5a0fabeaf4c42b411f6
PabloCasaniGaldon <pablocasani@gmail.com>
2024-02-12 16:15:38 +0100

    .

--- examples/stardist2D_example.py
+++ examples/stardist2D_example.py
@@ -1,7 +1,7 @@
 ### LOAD PACKAGE ###
 import sys
 sys.path.append('/home/pablo/Desktop/PhD/projects/embdevtools/src')
-from embdevtools import get_file_name, read_img_with_resolution, CellTracking, load_CellTracking, save_4Dstack, save_4Dstack_labels, norm_stack_per_z, compute_labels_stack, get_file_names
+from embdevtools import get_file_name, read_img_with_resolution, CellTracking, tif_reader_5D, save_4Dstack, save_4Dstack_labels, norm_stack_per_z, compute_labels_stack, get_file_names
 
 ### PATH TO YOU DATA FOLDER AND TO YOUR SAVING FOLDER ###
 path_data='/home/pablo/Desktop/PhD/projects/Data/blastocysts/Lana/20230607_CAG_H2B_GFP_16_cells/stack_2_channel_0_obj_bottom/crop/20230607_CAG_H2B_GFP_16_cells_stack2/'
