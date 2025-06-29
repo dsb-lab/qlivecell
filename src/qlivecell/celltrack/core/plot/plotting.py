4117e541c835b9bbb824b1376d0df387e5566ce6
Pablo Casani Galdon <pablocasani@gmail.com>
2025-06-29 14:21:22 +0200

    plot first 3 channels when channels not specified

--- src/qlivecell/celltrack/core/plot/plotting.py
+++ src/qlivecell/celltrack/core/plot/plotting.py
@@ -69,7 +69,9 @@ def check_stacks_for_plotting(
         ]
         channels = plot_args["channels"]
         if channels is None:
-            channels = [i for i in range(stacks_for_plotting.shape[2])]
+            # If no plotting channel is specified, the first three in the hyperstack are used for RGB plotting
+            channels = [i for i in range(min(stacks_for_plotting.shape[2], 3))]
+            
     plot_args["dim_change"] = plot_args["plot_stack_dims"][0] / stacks.shape[-2]
     plot_args["_plot_xyresolution"] = xyresolution * plot_args["dim_change"]
 
@@ -103,7 +105,7 @@ def check_stacks_for_plotting(
 
 def norm_stack_per_z(IMGS, saturation=0.7):
     IMGS_norm = np.zeros_like(IMGS)
-    saturation = 0.7 * 255
+    saturation = saturation * 255
     for t in range(IMGS.shape[0]):
         for z in range(IMGS.shape[1]):
             IMGS_norm[t, z] = (IMGS[t, z] / np.max(IMGS[t, z])) * saturation
